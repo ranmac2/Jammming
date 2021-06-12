223bff17c4716d5daffbd186f8444ff03df6ff7e
Randy <randy_maclean@hotmail.ca>
2021-06-12 15:55:05 -0700

    Added track removal functions and playlist name change functions

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -40,6 +40,8 @@ class App extends React.Component {
       ]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -49,7 +51,20 @@ class App extends React.Component {
     }
 
     tracks.push(track);
-    this.setState({playlistTracks: tracks});
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({
+      playlistName = name
+    })
   }
 
 
@@ -60,8 +75,16 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <SearchResults 
+            searchResults={this.state.searchResults} 
+            onAdd={this.addTrack} 
+            />
+            <Playlist 
+            playlistName={this.state.playlistName} 
+            playlistTracks={this.state.playlistTracks} 
+            onRemove={this.removeTrack} 
+            onNameChange={this.updatePlaylistName} 
+            />
           </div>
         </div>
       </div>
